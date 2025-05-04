3cb7ecef5d777cea070f0ad90f532cb0a7005ef2
Olgica Kalceva <kalcevaaolgica@gmail.com>
2025-05-04 23:23:27 +0200

    Add files via upload

---
+++ thirdHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace thirdHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = UserDatabase.Search(name: "Sofia");
+
+            foreach (var user in results)
+            {
+                Console.WriteLine($"Your Id: {user.Id}, your name: {user.Name}, and your age: {user.Age}");
+            }
+        }
+    }
+}
---
+++ thirdHomework/User.cs
@@ -0,0 +1,19 @@
+﻿namespace thirdHomework
+{
+   public class User
+    {
+        public User(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
+
