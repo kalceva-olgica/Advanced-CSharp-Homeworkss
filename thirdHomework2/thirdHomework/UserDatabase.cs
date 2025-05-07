e38368b1f6aec3e906a9e61657dfd2a74dd67de4
Olgica Kalceva <kalcevaaolgica@gmail.com>
2025-05-07 16:00:29 +0200

    Add files via upload

---
+++ thirdHomework2/thirdHomework/UserDatabase.cs
@@ -0,0 +1,34 @@
+﻿namespace thirdHomework
+{
+    public static class UserDatabase
+    {
+        public static List<User> Users = new List<User>
+    {
+        new User(1, "Sofia", 21),
+        new User(2, "Manuela", 44),
+        new User(3, "Aneta", 58)
+    };
+
+        public static List<User> Search(int? id = null, string name = null, int? age = null)
+        // '?' e nullable odnosno toa znaci i da ne stavime id nema da ni izvadi error tuku toa sto ke go stavime toa ke ni vrati
+        {
+            List<User> result = new List<User>();
+
+            foreach (var user in Users)
+            {
+                if ((id == null || user.Id == id) &&
+                    (name == null || user.Name == name) &&
+                    (age == null || user.Age == age))
+                {
+                    result.Add(user);
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+
+}
+
